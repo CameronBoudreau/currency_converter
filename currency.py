db544c73f6753fa5507efa680f8f67edc1329885
CameronB <cboudreau6238@gmail.com>
2016-04-04 22:49:03 -0400

    some minor changes

--- currency.py
+++ currency.py
@@ -5,7 +5,7 @@ class DifferentCurrencyCodeError(Exception):
 class Currency():
     def __init__(self, currency_code, amount= 0):
         self.amount = amount
-        self.currency_code = symbol_to_code(currency_code)
+        self.currency_code = self.symbol_to_code(currency_code)
 
     symbol_dict = {
     '$':'USD',
@@ -18,7 +18,7 @@ class Currency():
             currency_code = ''.join(i for i in currency_code if not i == '.' and not i.isalnum() or i.isspace())
             return symbol_dict[currency_code]
         else:
-            return currency_code
+            return currency_code.rstrip()
 
     def __eq__(self, other):
         return self.currency_code == other.currency_code and self.amount == other.amount
--- currency_converter.py
+++ currency_converter.py
@@ -4,41 +4,52 @@ class CurrencyConverter():
     def __init__(self, conversion_dict):
         self.conversion_dict = conversion_dict
 
-    def convert_currency(code_one, amount_one, code_two, amount_two):
+    conversion_dict = {
+    'USD':1.0,
+    'EUR':0.74,
+    'YEN': 149
+    }
+
+### FIX THIS ###
+    def convert(conversion_dict, code_one, amount, code_two):
+        print("Value of first dict code: ", conversion_dict[code_one])
+        print("Value of first second code: ", conversion_dict[code_two])
+
+        converted_amount = conversion_dict[code_one] * conversion_dict[code_two] * amount
+        return converted_amount
 
 
 
 
 
+conversion_dict = {
+'USD':1.0,
+'EUR':0.74,
+'YEN': 149
+}
 
-# def main():
-#     conversion_dict = {
-#     'USD':1.0,
-#     'EUR':0.74,
-#     'YEN': 149
-#     }
-#
-#     cc = CurrencyConverter(conversion_dict)
-#
-# currency_USD = Currency(5.5, 'USD')
-# currency_EUR = Currency(5.34, 'EUR')
-# currency_JPY = Currency(5, 'JPN')
-#
+currency_USD = Currency('USD', 5.5)
+currency_EUR = Currency('EUR', 5.34)
+currency_JPY = Currency('JPN', 5)
+
+
+test_converter = CurrencyConverter(conversion_dict)
+print(CurrencyConverter.convert(conversion_dict, currency_USD, 10, currency_EUR))
 # test = Currency("$7.50")
 # print(test.symbol_to_code('$7.50'))
-# # result = currency_one * currency_two
+# result = currency_one * currency_two
+
+# print("multiply same is type:", type(result))
+# print("multiply same is :", currency_one * currency_two)
+# print("multiply same float is :", currency_one * currency_two)
+# print("multiply object type: ", type(currency_one * currency_two))
 #
-# # print("multiply same is type:", type(result))
-# # print("multiply same is :", currency_one * currency_two)
-# # print("multiply same float is :", currency_one * currency_two)
-# # print("multiply object type: ", type(currency_one * currency_two))
-# #
-# # print("added ", currency_one + currency_two)
-# # # print("added with different code", currency_one + currency_three)
-# # print("sub ", currency_one - currency_two)
-# # # print("sub with different code", currency_one - currency_three)
-# # print('equal', currency_one == currency_two)
-# print('not equal', currency_USD != currency_EUR)
-#
-# if __name__ == '__main__':
-#     main()
+# print("added ", currency_one + currency_two)
+# # print("added with different code", currency_one + currency_three)
+# print("sub ", currency_one - currency_two)
+# # print("sub with different code", currency_one - currency_three)
+# print('equal', currency_one == currency_two)
+print('not equal', currency_USD != currency_EUR)
+
+if __name__ == '__main__':
+    main()
